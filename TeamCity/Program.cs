8cbf0ac27ec7189b0742b7fe73ea867db4183e43
Behnam Amiri <amiri.behnam4@gmail.com>
2016-01-09 17:01:53 -0700

    #1

---
+++ TeamCity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCity
+{
+    class Program
+    {
+        public static float firstNumber { get; set; }
+
+        public static float SecondNumber { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two number");
+            firstNumber =float.Parse(Console.ReadLine());
+            SecondNumber = float.Parse(Console.ReadLine());
+
+            bool x; 
+            float result = sum(firstNumber, SecondNumber);
+            if (result == ( firstNumber + SecondNumber ))
+            {
+                x = true;
+            }
+            else
+            {
+                x = false;
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        private static float sum(float first , float second)
+        {
+            return first + second;
+        }
+    }
+}
