45fc7e34ae1092ec63e8d4a739de0a87fd853ba8
Behnam <behnama@axosoft.com>
2017-02-03 14:49:03 -0700

    [axof: #22 ]

--- TeamCity/Program.cs
+++ TeamCity/Program.cs
@@ -15,6 +15,7 @@ namespace TeamCity
         static void Main(string[] args)
         {
 			//Test comment test
+			// new line
             Console.WriteLine("Enter two number");
             firstNumber =float.Parse(Console.ReadLine());
             SecondNumber = float.Parse(Console.ReadLine());
