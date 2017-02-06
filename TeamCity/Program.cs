7c305b8fa8549634c186ae141598fec754d7ca39
Behnam <behnama@axosoft.com>
2017-02-06 16:17:18 -0700

    This is da most important commit of all the time[axof: 9617]

--- TeamCity/Program.cs
+++ TeamCity/Program.cs
@@ -15,6 +15,7 @@ namespace TeamCity
         static void Main(string[] args)
         {
 			//one
+			// 54
             Console.WriteLine("Enter two number");
             firstNumber =float.Parse(Console.ReadLine());
             SecondNumber = float.Parse(Console.ReadLine());
