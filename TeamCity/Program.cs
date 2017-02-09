638dae1b701054412d439c910b7fa5ef7fbbc0ba
Brett Goldman <brettg@axosoft.com>
2017-02-09 13:19:20 -0700

    Wawa wee wa.... this a big laser [axof: 14]

--- TeamCity/Program.cs
+++ TeamCity/Program.cs
@@ -14,7 +14,7 @@ namespace TeamCity
 
         static void Main(string[] args)
         {
-		//test
+		//test1
             Console.WriteLine("Enter two number");
             firstNumber =float.Parse(Console.ReadLine());
             SecondNumber = float.Parse(Console.ReadLine());
